f372b4f36e35b9a55e1f16c814cea8763555a92a
gloofys <fred.brosman06@gmail.com>
2026-03-04 22:10:54 +0200

    Backend and frontend initial package structure

--- backend/src/main/java/com/example/cgi/api/controller/HealthController.java
+++ backend/src/main/java/com/example/cgi/api/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.example.cgi.api;
+package com.example.cgi.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
