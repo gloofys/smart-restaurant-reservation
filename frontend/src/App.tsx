bb71e581b3f7cde9a97b558622a8f169ce1c8c86
gloofys <fred.brosman06@gmail.com>
2026-03-04 21:55:56 +0200

    Initial frontend setup - React, Vite, Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="items-center justify-center">
+            <h1 className="text-3xl font-bold text-blue-600">
+                HELLLLLLLLLL
+            </h1>
+        </div>
+    )
+}
